b90d677f389e2b78623b1953646d43e70574e419
_Zinhle <zbuhlungu1@gmail.com>
2022-03-11 14:50:24 +0200

    fonts

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,6 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.hero_container}>
           <div className={styles.hero_content}>
-
             <h1 className={styles.title}>
               Creating
               the future
@@ -35,14 +34,12 @@ export default function Home() {
             </p>
           </div>
           <div className={styles.headerImg}>
-
             <Image
               src="/header-img-01.svg"
               width={700}
               height={700}
               alt='de novo illustration'
             />
-
           </div>
         </div>
         <div className={styles.gridTitle}>
@@ -50,7 +47,6 @@ export default function Home() {
             textAlign: "center"
           }}>Meet the Team</p>
         </div>
-
         <div className={styles.grid}>
           <div
             className={styles.card}>
@@ -61,17 +57,16 @@ export default function Home() {
                 height={300}
                 alt='Jean'
               />
-
             </div>
             <h2>Jean Louwrens </h2>
             <p
               style={{
                 textAlign: "center"
               }}>CEO</p>
-            <Accordion 
-            style={{
-              fontFamily: 'Monument Extended - light'
-            }}
+            <Accordion
+              style={{
+                fontFamily: 'Monument Extended - light'
+              }}
               title="+" content="<p>With a background in global trade, Jean is the guy conducting the businessey side of De Novo Dairy. When he's not working on his excel spreadsheets building his vision of using these tiny little organisms to make a big huge impact on the food system, he can be found in his natural habitat, the swimming pool.</p>" />
           </div>
           <div
@@ -88,12 +83,11 @@ export default function Home() {
             <p style={{
               textAlign: "center"
             }}>COO</p>
-            <Accordion 
-             style={{
-              fontFamily: 'Monument Extended - light'
-            }}
-            title="+" content="<p> Our resident engineer, Richard keeps De Novo Dairy's millions of micro factories running at full capacity. He's a DIY enthusiast and is probably the last man on earth that still reads a physical newspaper. He was last seen trying to instruct yeast cells on the benefits of matrix management.</p>" />
-
+            <Accordion
+              style={{
+                fontFamily: 'Monument Extended - light'
+              }}
+              title="+" content="<p> Our resident engineer, Richard keeps De Novo Dairy's millions of micro factories running at full capacity. He's a DIY enthusiast and is probably the last man on earth that still reads a physical newspaper. He was last seen trying to instruct yeast cells on the benefits of matrix management.</p>" />
           </div>
           <div
             className={styles.card}>
@@ -104,7 +98,6 @@ export default function Home() {
                 height={300}
                 alt='Joni Symon'
               />
-
             </div>
             <h2>Joni Symon</h2>
             <p
@@ -116,50 +109,42 @@ export default function Home() {
             <Accordion
               title="+"
               content=" <p>Joni is our biochemist and protein enthusiast. Favourite pastimes include cooking up milk proteins in the lab, suspending herself from climbing walls, and desperately seeking out some decent vegan pizza.</p>" />
-
           </div>
           <div
             className={styles.card}>
             <div className={styles.avatar}>
-
               <Image
                 src="/Leah.jpeg"
                 width={300}
                 height={300}
                 alt='JDr Leah Bessa'
               />
-
             </div>
             <h2>Dr Leah Bessa</h2>
             <p style={{
               textAlign: "center"
             }}>CSO</p>
             <Accordion title="+" content="<p>Leah, our very own mad food scientist, began her food journey dabbling in plant and insect protein. She is now on a mission to turn our proteins into new and exciting products. She's often got Frankie at her side, and is best known for fueling the team with delicious vegan cheesecake.</p>" />
-
           </div>
           <div
             className={styles.card}>
             <div className={styles.avatar}>
-
               <Image
                 src="/Hendrik.jpeg"
                 width={300}
                 height={300}
                 alt='Hendrik Els'
               />
-
             </div>
             <h2>Hendrik Els</h2>
             <p style={{
               textAlign: "center"
             }}>Senior Research Scientist</p>
             <Accordion title="+" content="<p>Hendrik, our molecular biologist and yeast micro-manager is working tirelessly in the lab constantly searching for new ways to manipulate tiny life forms through coffee-fueled brainstorming sessions to find the most efficient way of getting our hosts to produce the very best animal-free proteins.</p> " />
-
           </div>
           <div
             className={styles.card}>
             <div className={styles.avatar}>
-
               <Image
                 src="/Frankie.jpeg"
                 width={300}
@@ -201,10 +186,8 @@ export default function Home() {
               <li>
                 <Link
                   href="/"
-
                 >
                   <a>
-
                     <Image
                       src="/twitter.png"
                       width={30}
@@ -217,10 +200,8 @@ export default function Home() {
               <li>
                 <Link
                   href="/"
-
                 >
                   <a >
-
                     <Image
                       src="/instagram.png"
                       width={30}
@@ -241,7 +222,6 @@ export default function Home() {
                       height={30}
                       alt='mail'
                     />
-
                   </a>
                 </Link>
               </li>
