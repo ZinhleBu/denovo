9cef83c515f7c8f0a9ae061fc53d7c5221228b02
_Zinhle <zbuhlungu1@gmail.com>
2022-03-11 15:03:17 +0200

    removed ceo style

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,6 @@ export default function Home() {
             <h2>Jean Louwrens </h2>
             <p
               style={{
-                fontFamily:"Neue Montreal",
                 textAlign: "center"
               }}>CEO</p>
             <Accordion
